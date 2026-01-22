654696f06e68b903e4041a69bd1747094366cbf9
Dušan Milekić <dusanmilekic0511@gmail.com>
2026-01-22 19:33:26 +0100

    Update baseURL.ts

--- frontend/src/config/api/baseURL.ts
+++ frontend/src/config/api/baseURL.ts
@@ -1,8 +1,8 @@
-const URL = "http://127.0.0.1:8000";
+const URL = "https://musicvibe-production.up.railway.app/api";
 
 export default URL;
 
 //http://127.0.0.1:8000
 
 
-//https://musicvibe-production.up.railway.app/api
+//
